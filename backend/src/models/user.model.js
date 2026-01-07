b92ee149de72fc659931f01b229f8f0df5aa4ae1
Kabz Jose <josekabz00@gmail.com>
2026-01-07 14:34:58 +0300

    Added schema

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose,{Schema} from "mongoose";
+
+const userSchema= new Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            minlength:1,
+            malength:50
+        },
+        password:{
+            type:String,
+            required:true,
+            minlength:6,
+            maxlength:40,
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+export const User= mongoose.model("User",userSchema);

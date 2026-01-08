f81dc1713ea37481b8e033e3c18adf97bd59d1ec
Kabz Jose <josekabz00@gmail.com>
2026-01-09 00:19:48 +0300

    Added update api

--- backend/src/controller/post.controler.js
+++ backend/src/controller/post.controler.js
@@ -36,4 +36,27 @@ const getPosts=async(req,res)=>{
     }
 )}
 }
-export {createPost, getPosts};
+
+//update post
+const updatePost=async(req,res)=>{
+    try{
+      //basic validation to check if the body is  empty
+      if (Object.keys(req.body).length===0){
+        return res.status(400).json({message:"Request body cannot be empty"});
+      }
+      const post= await Post.findByIdAndUpdate(
+        req.params.id,req.body,{new:true});
+       if(!post){
+        return res.status(404).json({message:"Post not found"});
+ }
+    res.status(200).json({
+        message:"Post updated successfully",
+        post
+    });
+    }catch(error){
+       res.status(500).json({message:"internal server error",
+        error
+    })
+}
+}
+export {createPost, getPosts, updatePost};
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,10 +1,10 @@
 
 import { Router } from "express";
-import { createPost,getPosts } from "../controller/post.controler.js";
+import { createPost,getPosts,updatePost } from "../controller/post.controler.js";
 
 const router = Router();
 router.post("/create", createPost);
 router.get("/getPosts", getPosts);
-
+router.patch("/update/:id", updatePost);
 
 export default router;
